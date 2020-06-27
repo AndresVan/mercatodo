c3baafbaf3b9ec6f3500f82eaa15b104ccff55b5
Andrés Vanegas Castañeda <agvanegasc@gmail.com>
2020-06-26 23:20:41 -0500

    Feat: Configuración base de la página web

---
+++ database/migrations/2020_06_27_053605_create_column_users_in_admin.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateColumnUsersInAdmin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('admin_users', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('privileges');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('admin_users', function (Blueprint $table) {
+           $table->dropColumn('name','privileges','email','email_verified_at');
+        });
+    }
+}
